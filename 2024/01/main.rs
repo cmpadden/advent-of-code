2ee1c82288c10899dc9a9f1d2572763b7eeed300
Colton Padden <colton@dagsterlabs.com>
2024-12-02 11:17:15 -0500

    2024 - 01p2

--- 2024/01/main.rs
+++ 2024/01/main.rs
@@ -51,12 +51,20 @@ distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
 
 In the example list above, the pairs and distances would be as follows:
 
-    The smallest number in the left list is 1, and the smallest number in the right list is 3. The distance between them is 2.
-    The second-smallest number in the left list is 2, and the second-smallest number in the right list is another 3. The distance between them is 1.
+    The smallest number in the left list is 1, and the smallest number in the right list is 3. The
+    distance between them is 2.
+
+    The second-smallest number in the left list is 2, and the second-smallest number in the right
+    list is another 3. The distance between them is 1.
+
     The third-smallest number in both lists is 3, so the distance between them is 0.
+
     The next numbers to pair up are 3 and 4, a distance of 1.
+
     The fifth-smallest numbers in each list are 3 and 5, a distance of 2.
-    Finally, the largest number in the left list is 4, while the largest number in the right list is 9; these are a distance 5 apart.
+
+    Finally, the largest number in the left list is 4, while the largest number in the right list
+    is 9; these are a distance 5 apart.
 
 To find the total distance between the left list and the right list, add up the distances between
 all of the pairs you found. In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance
@@ -64,6 +72,57 @@ of 11!
 
 Your actual left and right lists contain many location IDs. What is the total distance between your
 lists?
+*/
+
+/*
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very
+different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's
+handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in
+both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted
+handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in
+the right list. Calculate a total similarity score by adding up each number in the left list after
+multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+    The first number in the left list is 3. It appears in the right list three times, so the
+    similarity score increases by 3 * 3 = 9.
+
+    The second number in the left list is 4. It appears in the right list once, so the similarity
+    score increases by 4 * 1 = 4.
+
+    The third number in the left list is 2. It does not appear in the right list, so the similarity
+    score does not increase (2 * 0 = 0).
+
+    The fourth number, 1, also does not appear in the right list.
+
+    The fifth number, 3, appears in the right list three times; the similarity score increases by
+    9.
+
+    The last number, 3, appears in the right list three times; the similarity score again increases
+    by 9.
+
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 +
+9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
 */
 
 /*
@@ -78,6 +137,7 @@ USAGE
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
     let path = Path::new("input.txt");
@@ -98,13 +158,29 @@ fn main() -> io::Result<()> {
     a.sort();
     b.sort();
 
-    let answer: i32  = a.iter().zip(b.iter()).map(|(&a, &b)| (a - b).abs()).collect::<Vec<i32>>().into_iter().sum();
+    let p1: i32  = a.iter().zip(b.iter()).map(|(&a, &b)| (a - b).abs()).collect::<Vec<i32>>().into_iter().sum();
 
-    println!("{}", answer);
+    println!("p1: {}", p1);
+
+    // Begin part 2
+
+    let mut counts = HashMap::new();
+
+    for _a in &a {
+        for _b in &b {
+            if &_a == &_b {
+                *counts.entry(_a).or_insert(0) += 1;
+            }
+        }
+    }
+
+    let mut p2: i32 = 0;
+    for (key, value) in &counts {
+        p2 += *key * value;
+    }
+
+    println!("p2: {}", p2);
 
     Ok(())
 }
-
-
-
 
