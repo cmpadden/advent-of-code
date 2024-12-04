5e56d6a3cce6524832276b0b9857fd6f60032955
Colton Padden <colton@dagsterlabs.com>
2024-12-04 16:40:19 -0500

    2024 - 04p2

--- 2024/04/main.rs
+++ 2024/04/main.rs
@@ -1,5 +1,4 @@
 /*
-
 --- Day 4: Ceres Search ---
 
 "Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the
@@ -48,7 +47,36 @@ S.S.S.S.SS
 .X.X.XMASX
 
 Take a look at the little Elf's word search. How many times does XMAS appear?
+*/
 
+/*
+--- Part Two ---
+
+The Elf looks quizzically at you. Did you misunderstand the assignment?
+
+Looking for the instructions, you flip over the word search to find that this isn't actually an
+XMAS puzzle; it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X.
+One way to achieve that is like this:
+
+M.S .A. M.S
+
+Irrelevant characters have again been replaced with . in the above diagram. Within the X, each MAS
+can be written forwards or backwards.
+
+Here's the same example from before, but this time all of the X-MASes have been kept instead:
+
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+
+In this example, an X-MAS appears 9 times.
 */
 
 /*
@@ -114,19 +142,48 @@ pub fn p1(input: &str) -> i32 {
     counter
 }
 
+pub fn p2(input: &str) -> i32 {
+    let grid: Vec<Vec<char>> = input.lines().map(|row| row.chars().collect()).collect();
+
+    let mut counter = 0;
+
+    for j in 0..grid.len() {
+        for i in 0..grid[j].len() {
+            if j + 2 < grid.len() && i + 2 < grid[j].len() {
+                let e1 = (grid[j][i], grid[j + 1][i + 1], grid[j + 2][i + 2]);
+                let e2 = (grid[j][i + 2], grid[j + 1][i + 1], grid[j + 2][i]);
+
+                if (e1 == ('M', 'A', 'S') || e1 == ('S', 'A', 'M'))
+                    && (e2 == ('M', 'A', 'S') || e2 == ('S', 'A', 'M'))
+                {
+                    counter += 1;
+                }
+            }
+        }
+    }
+
+    counter
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    static INPUT: &str = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX";
 
     #[test]
-    fn test_pt1() {
-        let input = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX";
-        assert_eq!(p1(&input), 18);
+    fn test_p1() {
+        assert_eq!(p1(&INPUT), 18);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(&INPUT), 9);
     }
 }
 
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("input.txt")?;
-    println!("{}", p1(&input));
+    println!("p1 {}", p1(&input));
+    println!("p2 {}", p2(&input));
     Ok(())
 }
