7a84f5706017330f535826e7009ae24017e9ff3e
Colton Padden <colton@dagsterlabs.com>
2024-12-05 13:47:04 -0500

    2024 - 05p2

--- 2024/05/main.rs
+++ 2024/05/main.rs
@@ -139,7 +139,66 @@ pub fn p1(input: &str) -> i32 {
     counter
 }
 
-pub static INPUT: &str = "47|53
+/*
+--- Part Two ---
+
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix
+the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in
+the right order. For the above example, here are the three incorrectly-ordered updates and their
+correct orderings:
+
+    75,97,47,61,53 becomes 97,75,47,61,53.
+    61,13,29 becomes 61,29,13.
+    97,13,75,29,47 becomes 97,75,47,29,13.
+
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page
+numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page
+numbers after correctly ordering just those updates?
+*/
+
+pub fn p2(input: &str) -> i32 {
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+
+    let mut counter = 0;
+
+    for (ix, mut update) in _parse(updates, ',').into_iter().enumerate() {
+        let mut fixed = false;
+        loop {
+            let mut updated_for_all_rules = false;
+            for rule in _parse(rules, '|') {
+                if update.contains(&rule[0]) && update.contains(&rule[1]) {
+                    let x1 = update.iter().position(|&x| x == rule[0]).unwrap();
+                    let x2 = update.iter().position(|&x| x == rule[1]).unwrap();
+                    if x1 > x2 {
+                        // swap records, and indicate that it has been updated
+                        update.swap(x1, x2);
+                        updated_for_all_rules = true;
+                        fixed = true;
+                    }
+                }
+            }
+
+            // loop until no updates were required
+            if !updated_for_all_rules {
+                break;
+            }
+        }
+
+        if fixed {
+            println!("{:?}", (ix, fixed));
+            counter += update[update.len() / 2];
+        }
+    }
+
+    counter
+}
+
+pub static INPUT: &str = "\
+47|53
 97|13
 97|61
 97|47
@@ -175,12 +234,13 @@ mod tests {
     #[test]
     fn test_p1() {
         assert_eq!(p1(&INPUT), 143);
+        assert_eq!(p2(&INPUT), 123);
     }
 }
 
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("input.txt")?;
     println!("p1 {}", p1(&input));
-    //println!("p2 {}", p2(&input));
+    println!("p2 {}", p2(&input));
     Ok(())
 }
