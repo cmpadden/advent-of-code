f2edada2f935784327c542b5879047eca097dbf0
Colton Padden <colton.padden@fastmail.com>
2025-12-14 13:54:40 -0500

    2025 - 06p2

--- 2025/06/main.py
+++ 2025/06/main.py
@@ -59,3 +59,58 @@ for column, operation in zip(transpose(lines), operations):
             raise Exception("Unsupported operation")
 
 acc
+
+"""
+--- Part Two ---
+
+The big cephalopods come back to check on how things are going. When they see that your grand total doesn't match the one expected by the worksheet, they realize they forgot to explain how to read cephalopod math.
+
+Cephalopod math is written right-to-left in columns. Each number is given in its own column, with the most significant digit at the top and the least significant digit at the bottom. (Problems are still separated with a column consisting only of spaces, and the symbol at the bottom of the problem is still the operator to use.)
+
+Here's the example worksheet again:
+
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+
+Reading the problems right-to-left one column at a time, the problems are now quite different:
+
+    The rightmost problem is 4 + 431 + 623 = 1058
+    The second problem from the right is 175 * 581 * 32 = 3253600
+    The third problem from the right is 8 + 248 + 369 = 625
+    Finally, the leftmost problem is 356 * 24 * 1 = 8544
+
+Now, the grand total is 1058 + 3253600 + 625 + 8544 = 3263827.
+
+Solve the problems on the math worksheet again. What is the grand total found by adding together all of the answers to the individual problems?
+"""
+
+lines = open("input.txt").readlines()
+
+numbers = lines[:-1]
+
+operations = lines[-1].split()
+
+
+numbers_fixed = []
+numbers_fixed_group = []
+for i in range(len(numbers[0])):
+    _n = ""
+    for j in range(len(numbers)):
+        _n += lines[j][i]
+    if not _n.strip():  # new column
+        numbers_fixed.append(numbers_fixed_group)
+        numbers_fixed_group = []
+    else:
+        numbers_fixed_group.append(int(_n.strip()))
+
+acc = 0
+for column, operation in zip(numbers_fixed, operations):
+    match operation:
+        case "*":
+            acc += math.prod(column)
+        case "+":
+            acc += sum(column)
+        case _:
+            raise Exception("Unsupported operation")
